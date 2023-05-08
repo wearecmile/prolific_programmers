27423cd18b98740e3a21aef97e28b52662f4a1c2
avani2803 <81511370+avani2803@users.noreply.github.com>
2023-05-08 18:13:01 +0530

    Added SwaggerConfig

--- src/main/java/com/cmile/meetmate/utils/constant/StringConstants.java
+++ src/main/java/com/cmile/meetmate/utils/constant/StringConstants.java
@@ -1,4 +1,13 @@
 package com.cmile.meetmate.utils.constant;
 
 public class StringConstants {
+
+    //Swagger Constants
+    public static final String TEXT_SWAGGER_TITLE = "All meet mate APIs";
+    public static final String TEXT_SWAGGER_DESC = "REST APIs reference for developers";
+    public static final String TEXT_SWAGGER_CONTACT_EMAIL = "cmile@gmail.com";
+    public static final String TEXT_SWAGGER_LICENCE = "http://licence-url.com";
+    public static final String TEXT_APP_NAME = "meet mate";
+    public static final String TEXT_SWAGGER_API_VERSION = "1.0";
+    public static final String TEXT_SWAGGER_REGEX_PATH = "/*.*";
 }
